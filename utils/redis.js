ef3d9c97677685cfaab4b6fee381fb378a1fce12
gumquat <gumquat@gmail.com>
2024-03-18 12:52:01 -0500

    eslinter

--- utils/redis.js
+++ utils/redis.js
@@ -1,11 +1,11 @@
 const redis = require('redis');
 
 class RedisClient {
-constructor() {
+  constructor() {
     this.client = redis.createClient();
 
     this.client.on('error', (err) => {
-        console.log(`Redis Client Error', ${err}`);
+      console.log(`Redis Client Error', ${err}`);
     });
     // this.client.connect();
     this.client.connected = true;
@@ -16,41 +16,41 @@ isAlive() {
 }
 
 async get(key) {
-    return new Promise((resolve, reject) => {
-        this.client.get(key, (err, value) => {
-        if (err) {
-            reject(err);
-        } else {
-            resolve(value);
-        }
-        });
+  return new Promise((resolve, reject) => {
+    this.client.get(key, (err, value) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(value);
+      }
     });
+  });
 }
 
 async set(key, value, duration) {
-    return new Promise((resolve, reject) => {
-        // this.client.setEx(key, duration, value, (err, reply) => {
-        this.client.set(key, value, 'EX', duration, (err, reply) => {
-        if (err) {
-            reject(err);
-        } else {
-            // resolve(reply);
-            resolve();
-        }
-        });
+  return new Promise((resolve, reject) => {
+    // this.client.setEx(key, duration, value, (err, reply) => {
+    this.client.set(key, value, 'EX', duration, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        // resolve(reply);
+        resolve();
+      }
     });
+  });
 }
 
 async del(key) {
-    return new Promise((resolve, reject) => {
-        this.client.del(key, (err) => {
-        if (err) {
-            reject(err);
-        } else {
-            resolve();
-        }
-        });
-    });
+  return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+      });
+  });
 }
 }
 

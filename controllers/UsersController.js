f1dbed26bb4cd7adab1fab7acfa2df60aac15775
gumquat <gumquat@gmail.com>
2024-03-18 15:08:37 -0500

    client

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,7 +2,7 @@
 // Controller file
 
 // Import the dbClient and redisClient
-const redis = require('redis');
+const redisClient = require('redis');
 
 const dbClient = require('../utils/db');
 const sha1 = require('../node_modules/sha1');

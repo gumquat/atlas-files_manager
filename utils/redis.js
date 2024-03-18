595077df33f262a636f1f09defe48705a24c32fd
gumquat <gumquat@gmail.com>
2024-03-18 12:37:33 -0500

    redis util changes

--- utils/redis.js
+++ utils/redis.js
@@ -1,7 +1,9 @@
 const redis = require('redis');
+
 class RedisClient {
 constructor() {
   this.client = redis.createClient();
+
   this.client.on('error', (err) => {
     console.log('Redis Client Error', err);
   });
@@ -9,16 +11,16 @@ constructor() {
 }
 
 isAlive() {
-  return this.client.isOpen;
+  return this.client.connected;
 }
 
 async get(key) {
   return new Promise((resolve, reject) => {
-    this.client.get(key, (err, value) => {
+    this.client.get(key, (err, reply) => {
       if (err) {
         reject(err);
       } else {
-        resolve(value);
+        resolve(reply);
       }
     });
   });
@@ -26,7 +28,8 @@ async get(key) {
 
 async set(key, value, duration) {
   return new Promise((resolve, reject) => {
-    this.client.setEx(key, duration, value, (err, reply) => {
+    // this.client.setEx(key, duration, value, (err, reply) => {
+    this.client.set(key, value, 'EX', duration, (err, reply) => {
       if (err) {
         reject(err);
       } else {

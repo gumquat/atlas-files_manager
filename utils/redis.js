0f16cc7c48caade02511e87d0b0fb7c8e3dce045
gumquat <gumquat@gmail.com>
2024-03-18 12:48:16 -0500

    eslint fix

--- utils/redis.js
+++ utils/redis.js
@@ -2,55 +2,55 @@ const redis = require('redis');
 
 class RedisClient {
 constructor() {
-  this.client = redis.createClient();
+    this.client = redis.createClient();
 
-  this.client.on('error', (err) => {
-    console.log(`Redis Client Error', ${err}`);
-  });
-  // this.client.connect();
-  this.client.connected = true;
+    this.client.on('error', (err) => {
+        console.log(`Redis Client Error', ${err}`);
+    });
+    // this.client.connect();
+    this.client.connected = true;
 }
 
 isAlive() {
-  return this.client.connected;
+    return this.client.connected;
 }
 
 async get(key) {
-  return new Promise((resolve, reject) => {
-    this.client.get(key, (err, value) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(value);
-      }
+    return new Promise((resolve, reject) => {
+        this.client.get(key, (err, value) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve(value);
+        }
+        });
     });
-  });
 }
 
 async set(key, value, duration) {
-  return new Promise((resolve, reject) => {
-    // this.client.setEx(key, duration, value, (err, reply) => {
-    this.client.set(key, value, 'EX', duration, (err, reply) => {
-      if (err) {
-        reject(err);
-      } else {
-        // resolve(reply);
-        resolve();
-      }
+    return new Promise((resolve, reject) => {
+        // this.client.setEx(key, duration, value, (err, reply) => {
+        this.client.set(key, value, 'EX', duration, (err, reply) => {
+        if (err) {
+            reject(err);
+        } else {
+            // resolve(reply);
+            resolve();
+        }
+        });
     });
-  });
 }
 
 async del(key) {
-  return new Promise((resolve, reject) => {
-    this.client.del(key, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
+    return new Promise((resolve, reject) => {
+        this.client.del(key, (err) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve();
+        }
+        });
     });
-  });
 }
 }
 

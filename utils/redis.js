fb8ebf684f421fe14dd8a59ed2c79a847b0273de
gumquat <gumquat@gmail.com>
2024-03-18 12:54:47 -0500

    eslint please let me get the point

--- utils/redis.js
+++ utils/redis.js
@@ -9,49 +9,49 @@ class RedisClient {
     });
     // this.client.connect();
     this.client.connected = true;
-}
+  }
 
-isAlive() {
+  isAlive() {
     return this.client.connected;
-}
+  }
 
-async get(key) {
-  return new Promise((resolve, reject) => {
-    this.client.get(key, (err, value) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(value);
-      }
-    });
-  });
-}
-
-async set(key, value, duration) {
-  return new Promise((resolve, reject) => {
-    // this.client.setEx(key, duration, value, (err, reply) => {
-    this.client.set(key, value, 'EX', duration, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        // resolve(reply);
-        resolve();
-      }
-    });
-  });
-}
-
-async del(key) {
-  return new Promise((resolve, reject) => {
-      this.client.del(key, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
       });
-  });
-}
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      // this.client.setEx(key, duration, value, (err, reply) => {
+      this.client.set(key, value, 'EX', duration, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          // resolve(reply);
+          resolve();
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+        this.client.del(key, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+        });
+    });
+  }
 }
 
 const redisClient = new RedisClient();

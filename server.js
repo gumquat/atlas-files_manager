2b366ea5864b17b6d1bd96450b2c979bcc6264ef
F-O-P <tayler.coon@gmail.com>
2024-03-18 22:44:30 -0500

    uncomment the line that imports routes

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 //import express and create an instance of the express application
 const express = require('express');
 const app = express();
+
 //Define a 'Port' variable, defaults to 5000
 const PORT = process.env.PORT || 5000;
-//import routes module from index file
-//const routes = require('./routes/index');
 
+//import routes module from index file
+const routes = require('./routes/index');
 
 // Load all routes
 app.use('/', routes);
+
 // Start the server by listening to the port
 app.listen(PORT, () => {
   // Callback func that logs a message when server is running

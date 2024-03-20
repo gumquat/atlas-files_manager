35957ec1cea7ae7f5245624b42716ac9d6599e14
F-O-P <tayler.coon@gmail.com>
2024-03-20 12:45:59 -0500

    updated spaceing on usercontroller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -8,7 +8,6 @@ const dbClient = require('../utils/db');
 const sha1 = require('../node_modules/sha1');
 
 class UsersController {
-  
   static async postNew(req, res) {
     const { email, password } = req.body;
 
@@ -22,25 +21,25 @@ class UsersController {
       return res.status(400).json({ error: 'Missing password' });
     }
 
-      // Check if email already exists in DB
-      const userExists = await dbClient.getUserByEmail(email);
-      if (userExists) {
-        return res.status(400).json({ error: 'Already exist' });
-      }
+    // Check if email already exists in DB
+    const userExists = await dbClient.getUserByEmail(email);
+    if (userExists) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
 
-      // Hash the password using SHA1
-      const hashedPassword = sha1(password);
-      // Create the new user
-      const newUser = {
-        email,
-        password: hashedPassword,
-      };
+    // Hash the password using SHA1
+    const hashedPassword = sha1(password);
+    // Create the new user
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
 
-      // Save the new user in the collection users
-      const savedUser = await dbClient.createUser(newUser);
+    // Save the new user in the collection users
+    const savedUser = await dbClient.createUser(newUser);
 
-      // Return the new user with only the email and the id
-      return res.status(201).json({ email: savedUser.email, id: savedUser._id });
+    // Return the new user with only the email and the id
+    return res.status(201).json({ email: savedUser.email, id: savedUser._id });
   }
 
   static async getMe(req, res) {

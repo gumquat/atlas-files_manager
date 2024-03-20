d64e22bd8c5e3527d4c75bab072691a4d2bf83d1
gumquat <gumquat@gmail.com>
2024-03-20 14:09:20 -0500

    commenting our authcontroller in index.js

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,9 @@ router.get('/status', AppController.getStatus);
 // log number of users and files there are
 router.get('/stats', AppController.getStats);
 // // sign in and authentication token creation for user
-router.get('/connect', AuthController.getConnect);
+// router.get('/connect', AuthController.getConnect);
 // // sign the user off
-router.get('/disconnect', AuthController.getDisconnect);
+// router.get('/disconnect', AuthController.getDisconnect);
 // retrieve identified user by token
 router.get('/users/me', UsersController.getMe);
 // // retrieve file by ID

ff7b830d7c0dc5f049a78785184c22a5861a8b09
F-O-P <tayler.coon@gmail.com>
2024-03-20 13:01:02 -0500

    updating index.js

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const AppController = require('../controllers/AppController');
 
 const UsersController = require('../controllers/UsersController');
-// const AuthController = require('../controllers/AuthController');
+const AuthController = require('../controllers/AuthController');
 // const FilesController = require('../controllers/FilesController');
 
 // API Endpoints
@@ -16,9 +16,9 @@ router.get('/status', AppController.getStatus);
 // log number of users and files there are
 router.get('/stats', AppController.getStats);
 // // sign in and authentication token creation for user
-// router.get('/connect', AuthController.getConnect);
+router.get('/connect', AuthController.getConnect);
 // // sign the user off
-// router.get('/disconnect', AuthController.getDisconnect);
+router.get('/disconnect', AuthController.getDisconnect);
 // retrieve identified user by token
 router.get('/users/me', UsersController.getMe);
 // // retrieve file by ID

a22b8f6326a117bb4d4b7dc98941fec06d8300f9
F-O-P <tayler.coon@gmail.com>
2024-03-19 21:29:31 -0500

    defined ObjectID, added if guards, put connect into it's own async function

--- utils/db.js
+++ utils/db.js
@@ -1,30 +1,40 @@
-
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 class DBClient {
   constructor() {
     // host, port, and database
-    const DB_HOST = process.env.DB_HOST || 'localhost';
-    const DB_PORT = process.env.DB_PORT || 27017;
-    const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+    // const DB_HOST = process.env.DB_HOST || 'localhost';
+    // const DB_PORT = process.env.DB_PORT || 27017;
+    // const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 
     // Connection URL
     // DO NOT USE THIS PART (unless working locally)
-    const url = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+    // const url = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
     const uri = "mongodb+srv://evannewman:<!!!!!!!!!PASSWORDHERE!!!!!!!>@evannewmanchock-cluster.j599tmt.mongodb.net/?retryWrites=true&w=majority&appName=EvanNewmanChock-CLUSTER"
     // Create a new MongoClient
-    this.client = new MongoClient(uri, { useUnifiedTopology: true });
+    this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     this.db = null;
 
     // Connect to the MongoDB server
-    this.client.connect((err) => {
-      if (err) {
-        console.error('Error connecting to MongoDB:', err);
-      } else {
-        console.log('Connected to MongoDB');
-      }
-    });
+    // this.client.connect((err) => {
+    //   if (err) {
+    //     console.error('Error connecting to MongoDB:', err);
+    //   } else {
+    //     console.log('Connected to MongoDB');
+    //   }
+    // });
+    this.connect();
+  }
+
+  async connect() {
+    try {
+      await this.client.connect();
+      console.log('Connected to MongoDB');
+      this.db = this.client.db();
+    } catch (err) {
+      console.error('Error connecting to MongoDB:', err);
+    }
   }
 
   isAlive() {
@@ -34,25 +44,37 @@ class DBClient {
 
   async nbUsers() {
     // Get the 'users' collection
-    const usersCollection = this.client.db().collection('users');
+    // const usersCollection = this.client.db().collection('users');
 
     // Count the number of documents in the collection
-    const usersCount = await usersCollection.countDocuments();
+    // const usersCount = await usersCollection.countDocuments();
 
+    // return usersCount;
+    if (!this.db) {
+      console.error('Database connection not established');
+      return 0;
+    }
+    const usersCount = await this.db.collection('users').countDocuments();
     return usersCount;
   }
 
   async nbFiles() {
     // Get the 'files' collection
-    const filesCollection = this.client.db().collection('files');
+    // const filesCollection = this.client.db().collection('files');
 
     // Count the number of documents in the collection
-    const filesCount = await filesCollection.countDocuments();
+    // const filesCount = await filesCollection.countDocuments();
 
+    // return filesCount;
+    if (!this.db) {
+      console.error('Database connection not established');
+      return 0;
+    }
+    const filesCount = await this.db.collection('files').countDocuments();
     return filesCount;
   }
 
-  //MISC BELOW//
+  // MISC BELOW
 
   // get user by email
   async getUserByEmail(email) {
@@ -62,9 +84,7 @@ class DBClient {
 
   // get user by id
   async getUserById(userId) {
-    if (!this.db) {
-      return null;
-    }
+    if (!this.isAlive()) return null;
     // Convert the userId string to an ObjectId for MongoDB
     const objectId = new ObjectId(userId);
     return this.db.collection('users').findOne({ _id: objectId });

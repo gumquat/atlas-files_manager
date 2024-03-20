61d5aa3feff1b578c0976b838613a90c9c8751f1
gumquat <gumquat@gmail.com>
2024-03-20 14:23:14 -0500

    AuthController - made into a class and changed variable named for readability

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,52 +1,55 @@
 const sha1 = require('sha1');
 const { v4: uuidv4 } = require('uuid');
-const MongoUtil = require('../utils/db');
-const RedisUtil = require('../utils/redis');
+const dbUtil = require('../utils/db');
+const redisUtil = require('../utils/redis');
 
 // GET /connect endpoint
-const connect = async (req, res) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).json({ error: 'Unauthorized' });
+class AuthController {
+  static async getConnect(req, res) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString();
+    const [email, password] = credentials.split(':');
+
+    const user = await dbUtil.findUser(email);
+    if (!user || user.password !== sha1(password)) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const token = uuidv4();
+    try {
+      await redisUtil.redisClient.set(`auth_${token}`, user._id.toString(), 'EX', 24 * 60 * 60);
+      return res.status(200).json({ token });
+    } catch (error) {
+      console.error('Error setting token in Redis:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+
+  static async disconnect(req, res) {
+    const token = req.headers['x-token'];
+    const userId = await redisUtil.redisClient.get(`auth_${token}`);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    try {
+      await redisUtil.redisClient.del(`auth_${token}`);
+      return res.status(204).end();
+    } catch (error) {
+      console.error('Error deleting token in Redis:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
+}
 
-  const base64Credentials = authHeader.split(' ')[1];
-  const credentials = Buffer.from(base64Credentials, 'base64').toString();
-  const [email, password] = credentials.split(':');
-
-  const user = await MongoUtil.findUser(email);
-  if (!user || user.password !== sha1(password)) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-
-  const token = uuidv4();
-  try {
-    await RedisUtil.redisClient.set(`auth_${token}`, user._id.toString(), 'EX', 24 * 60 * 60);
-    return res.status(200).json({ token });
-  } catch (error) {
-    console.error('Error setting token in Redis:', error);
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-
-const disconnect = async (req, res) => {
-  const token = req.headers['x-token'];
-  const userId = await RedisUtil.redisClient.get(`auth_${token}`);
-
-  if (!userId) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-
-  try {
-    await RedisUtil.redisClient.del(`auth_${token}`);
-    return res.status(204).end();
-  } catch (error) {
-    console.error('Error deleting token in Redis:', error);
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-
-module.exports = {
-  connect,
-  disconnect,
-};
+// module.exports = {
+//   connect,
+//   disconnect,
+// };
+module.exports = AuthController;

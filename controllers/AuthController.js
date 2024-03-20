24cb29c302aff3bfcc5046451336398a0ed515eb
F-O-P <tayler.coon@gmail.com>
2024-03-20 13:25:06 -0500

    removed atob

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,3 @@
-import { atob } from 'atob';
-
 const sha1 = require('sha1');
 const { v4: uuidv4 } = require('uuid');
 const MongoUtil = require('../utils/db');
@@ -13,18 +11,22 @@ const connect = async (req, res) => {
   }
 
   const base64Credentials = authHeader.split(' ')[1];
-  const credentials = atob(base64Credentials);
+  const credentials = Buffer.from(base64Credentials, 'base64').toString();
   const [email, password] = credentials.split(':');
 
-  const user = await MongoUtil.findUser(email);
-  if (!user || user.password !== sha1(password)) {
-    return res.status(401).json({ error: 'Unauthorized' });
+  try {
+    const user = await MongoUtil.findUser(email);
+    if (!user || user.password !== sha1(password)) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const token = uuidv4();
+    await RedisUtil.redisClient.set(`auth_${token}`, user._id.toString(), EX, 24 * 60 * 60);
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
-
-  const token = uuidv4();
-  await RedisUtil.redisClient.set(`auth_${token}`, user._id.toString(), EX, 24 * 60 * 60);
-
-  return res.status(200).json({ token });
 };
 
 const disconnect = async (req, res) => {

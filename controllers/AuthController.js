b6bdfecaccd21f2ec99d9adc60f9b0deba323efe
gumquat <gumquat@gmail.com>
2024-03-20 19:26:00 -0500

    added ascii

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -12,7 +12,7 @@ class AuthController {
     }
 
     const base64Credentials = authHeader.split(' ')[1];
-    const credentials = Buffer.from(base64Credentials, 'base64').toString();
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [email, password] = credentials.split(':');
 
     // const user = await dbUtil.findUser(email);

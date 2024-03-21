1a8e77da17102c5284a241e8c4e26f08f232509c
gumquat <gumquat@gmail.com>
2024-03-21 09:54:09 -0500

    uncomment index

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
-// const FilesController = require('../controllers/FilesController');
+const FilesController = require('../controllers/FilesController');
 
 // API Endpoints
 
@@ -22,14 +22,14 @@ router.get('/disconnect', AuthController.getDisconnect);
 // retrieve identified user by token
 router.get('/users/me', UsersController.getMe);
 // // retrieve file by ID
-// router.get('/files/:id', FilesController.getShow);
+router.get('/files/:id', FilesController.getShow);
 // // retrieve all files
-// router.get('/files', FilesController.getIndex);
+router.get('/files', FilesController.getIndex);
 
 // POST routes
 // create new user
 router.post('/users', UsersController.postNew);
 // // retrieve file by body data
-// router.post('/files', FilesController.postUpload);
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
